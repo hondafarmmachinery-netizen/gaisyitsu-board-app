88690972fb9bf9794a6e03eda51989928c380672
hondafarmmachinery-netizen <honda.farm.machinery@gmail.com>
2026-07-20 15:21:45 +0900

    add types

--- lib/types.ts
+++ lib/types.ts
@@ -33,3 +33,16 @@ export type State = {
   returnTime: string;
   updatedAt: string;
 };
+
+export type Member = {
+  id: number;
+  name: string;
+};
+
+export type State = {
+  memberId: number;
+  name: string;
+  location: string;
+  returnTime: string;
+  updatedAt: string;
+};
